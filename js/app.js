b0609a70c8d598dd35567ee05901003654bae00b
jpecht <me.jefferson@gmail.com>
2016-02-03 12:57:24 -0500

    updates based on feedback, more input/outputs

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var App = App || {};
 
 (function() {
 	App.initialize = function() {
-
+		$('.header-title').click(function() { hasher.setHash(''); });
 	};
 	
 	App.initHome = function() {
@@ -18,33 +18,25 @@ var App = App || {};
 			.property('value', function(d, i) { return Util.percentize(popAgeData[i].value); })
 			.on('change', function(d, i) {
 				popAgeData[i].value = Util.formatInputVal(this);
-				adjustPopAgeSum(i);
+				checkPopAgeSum();
 				updatePopAgeChart();
 			});
 		
-		var adjustPopAgeSum = function(indexChanged) {
+		var checkPopAgeSum = function() {
 			var sum = 0;
 			for (var i = 0; i < popAgeData.length; i++) sum += popAgeData[i].value;
-			var diff = sum - 1;
-			for (var j = 0; j < popAgeData.length; j++) {
-				if (j !== indexChanged) {
-					if (popAgeData[j].value >= diff) {
-						popAgeData[j].value -= diff;
-						$('.pop-age-table input').eq(j).val(Util.percentize(popAgeData[j].value));
-						break;
-					} else {
-						diff -= popAgeData[j].value;
-						popAgeData[j].value = 0;
-						$('.pop-age-table input').eq(j).val(Util.percentize(0));
-					}
-				}
+			$('.pop-age-table tbody tr:last-child td:nth-child(2)').text(Util.percentize(sum));
+			if (Math.abs(sum - 1) < 0.001) {
+				$('.pop-age-warning').slideUp();
+			} else {
+				$('.pop-age-warning').slideDown();
 			}
 		};
 		
 		// build bar chart for the population age distribution
-		var margin = {top: 30, right: 20, bottom: 30, left: 80};
-		var width = 450 - margin.left - margin.right;
-		var height = 200 - margin.top - margin.bottom;
+		var margin = {top: 45, right: 20, bottom: 25, left: 100};
+		var width = 420 - margin.left - margin.right;
+		var height = 220 - margin.top - margin.bottom;
    		var chart = d3.select('.pop-age-chart')
    			.attr('width', width + margin.left + margin.right)
    			.attr('height', height + margin.top + margin.bottom)
@@ -53,7 +45,7 @@ var App = App || {};
 
 		var x = d3.scale.ordinal()
 			.domain(popAgeData.map(function(d) { return d.age; }))
-			.rangeRoundBands([0, width], 0.3);
+			.rangeRoundBands([0, width], 0.2);
 		var xAxis = d3.svg.axis().scale(x)
 			.orient('bottom');
 		var xAxisG = chart.append('g')
@@ -74,22 +66,8 @@ var App = App || {};
 		var yAxisG = chart.append('g')
 			.attr('class', 'y axis')
 			.call(yAxis);
-			
-		var line = d3.svg.line()
-			.x(function(d) { return x(d.age); })
-			.y(function(d) { return y(d.value); })
-			.interpolate('basis');
-		var lineElement = chart.append('path')
-			.attr('class', 'line')
-			.attr('transform', 'translate(' + x.rangeBand()/2 + ',0)');
-		
-		
-		var updatePopAgeChart = function() {
-			lineElement
-				.datum(popAgeData)
-				.attr('d', line);
 				
-			// bars
+		var updatePopAgeChart = function() {
 			var bars = chart.selectAll('rect')
 				.data(popAgeData);
 			var newBars = bars.enter().append('rect');
@@ -101,6 +79,35 @@ var App = App || {};
 			bars.exit().remove();
 		};
 		updatePopAgeChart();
+		
+		
+		// multiselect for age range
+		$('.schisto-age-select').multiselect();
+		
+		
+		// malaria transmission pattern
+		$('.malaria-timing-select').on('change', function() {
+			if ($(this).val() === 'seasonal') $('.malaria-month-container').slideDown();
+			else $('.malaria-month-container').slideUp();
+		});
+		$('.malaria-month-select').multiselect();
+		
+		
+		// checkboxes for IRS and ITN
+		$('.irs-checkbox').on('change', function() {
+			$('.irs-true-contents').slideToggle();
+			updateDistributionBlock();
+		});
+		$('.itn-checkbox').on('change', function() {
+			$('.itn-true-contents').slideToggle();
+			updateDistributionBlock();
+		});
+		var updateDistributionBlock = function() {
+			var show = $('.irs-checkbox').is(':checked') && $('.itn-checkbox').is(':checked');
+			if (show) $('.input-subsection[name="distribution-strategy"]').slideDown();
+			else $('.input-subsection[name="distribution-strategy"]').slideUp();
+		};
+		
 		
 		// submit button
 		$('.input-submit-button').click(function() { hasher.setHash('output'); });
@@ -126,7 +133,7 @@ var App = App || {};
 		];
 		
 		// build bar chart for the population age distribution
-		var margin = {top: 30, right: 20, bottom: 30, left: 80};
+		var margin = {top: 30, right: 20, bottom: 80, left: 80};
 		var width = 650 - margin.left - margin.right;
 		var height = 250 - margin.top - margin.bottom;
    		var chart = d3.select('.output-bar-chart')
@@ -162,10 +169,11 @@ var App = App || {};
 		// bars
 		var bars = chart.selectAll('rect')
 			.data(barData);
-		var newBars = bars.enter().append('rect');
+		var newBars = bars.enter().append('rect')
+			.attr('class', 'bar');
 			
 		bars
-			.style('fill', function(d) { return (d.type === 'with integration') ? '#31a354' : 'steelblue'; })
+			.style('fill', function(d) { return (d.type === 'without integration') ? 'url(#diagonal-stripe-1)' : 'steelblue'; })
 			.attr('x', function(d) {
 				var xVal = x(d.disease) - 20;
 				if (d.type === 'with integration') xVal += 40;
@@ -175,6 +183,22 @@ var App = App || {};
 			.attr('width', x.rangeBand())
 			.attr('height', function(d) { return height - y(d.value); });
 		bars.exit().remove();
+		
+		// add legend
+		var legend = chart.append('g')
+			.attr('class', 'legend')
+			.attr('transform', 'translate(100,' + (height+50) + ')');
+		var legendGroups = legend.selectAll('g')
+			.data(Util.getUnique(barData.map(function(d) { return d.type; })))
+			.enter().append('g')
+				.attr('transform', function(d, i) { return 'translate(' + (200*i) + ')'; });
+		legendGroups.append('rect')
+			.attr('width', 15)
+			.attr('height', 15)
+			.style('fill', function(d) { return (d === 'without integration') ? 'url(#diagonal-stripe-1)' : 'steelblue'; });
+		legendGroups.append('text')
+			.attr('transform', 'translate(25,13)')
+			.text(function(d) { return d; });
 	};
 	
 	App.initialize();
