fea9b0786c80b2a5fd6a1f0ec1e7ad3c52baccdc
jpecht <me.jefferson@gmail.com>
2016-02-24 15:09:43 -0500

    testing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var app = require('express')();
+var http = require('http');
+http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('Testing... this should work.');
+	response.end();
+}).listen(process.env.PORT || 8888);
+
+var express = require('express');
+
+/*var app = require('express')();
 var server = require('http').Server(app);
 var path = require('path');
 var PythonShell = require('python-shell');
-
 
 // HTTP Server feedback behavior
 app.get('/', function(req, res) {
@@ -26,4 +34,4 @@ PythonShell.run('test.py', {
 }, function(err, results) {
 	if (err) throw err;
 	console.log(results);
-});
+});*/
