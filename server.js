30799db6d6fa5cfd7025cd67d56da55db2dfe440
jpecht <me.jefferson@gmail.com>
2016-02-24 14:44:14 -0500

    testing running py script

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-var http = require('http');
+var app = require('express')();
+var server = require('http').Server(app);
+var path = require('path');
+var PythonShell = require('python-shell');
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Test NodeJS Server');
-	response.end();
-}).listen(process.env.PORT || 8888);
+
+// HTTP Server feedback behavior
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+app.get(/^(.+)$/, function(req, res) {
+	res.sendFile(path.join(__dirname, '/', req.params[0]));
+});	
+
+// Start the HTTP Server
+server.listen(process.env.PORT || 8888, function() {
+	console.log('Server set up!');
+	console.log(server.address());
+});
+
+PythonShell.run('test.py', {
+	mode: 'text',
+	pythonOptions: ['-u'],
+	scriptPath: 'py',
+	args: ['testinput']
+}, function(err, results) {
+	if (err) throw err;
+	console.log(results);
+});
