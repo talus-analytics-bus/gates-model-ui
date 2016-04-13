ec3067acc3e2635e8aa723ce0b3cea52840d765a
jpecht <me.jefferson@gmail.com>
2016-04-13 17:29:15 -0400

    rearrangement and rewording of output

--- js/pages/output.js
+++ js/pages/output.js
@@ -8,46 +8,66 @@ var App = App || {};
 			return false;
 		}
 		
-		// prevalence percentages for each disease for integrated and non-integrated
-		var schistoPrevInt = App.outputs.integrated.schisto;
-		var malariaPrevInt = App.outputs.integrated.malaria;
-		var schistoPrevNoInt = App.outputs.separate.schisto;
-		var malariaPrevNoInt = App.outputs.separate.malaria;
-		
-		// determine recommendation
+		// define variables
 		var isSeasonal = (App.inputs.malaria_timing === 'seasonal');
-		var isRecommended = malariaPrevInt < malariaPrevNoInt;
+
+
+		// determine recommendation
+		var isRecommended = (App.outputs.separate.malaria - App.outputs.integrated.malaria >= 0.05);
 		var recOutput = isRecommended ? App.outputs.integrated : App.outputs.separate;
 
-
 		// update recommendation text
-		d3.select('.output-recommendation')
+		d3.select('.output-recommendation-text')
 			.text(isRecommended ? 'INTEGRATED INTERVENTIONS' : 'NON-INTEGRATED INTERVENTIONS')
 			.classed('text-success', isRecommended);
-		
-		// fill table showing prevalence
-		var data = [
-			{type: 'without integration', schisto: schistoPrevNoInt, malaria: malariaPrevNoInt},
-			{type: 'with integration', schisto: schistoPrevInt, malaria: malariaPrevInt}
-		];
-		d3.selectAll('.output-table tbody tr').each(function(d, i) {
-			d3.select(this).select('td:nth-child(2)')
-				.text(Util.percentize(data[i].schisto));
-			d3.select(this).select('td:nth-child(3)')
-				.text(Util.percentize(data[i].malaria))
-				.classed('text-success', function() { if (i === 1) return (malariaPrevInt < malariaPrevNoInt); })
-				.classed('text-danger', function() { if (i === 1) return (malariaPrevInt > malariaPrevNoInt); });
+			
+		// attach tooltip to question mark beside rec text
+		var nonIntContentStr = '<b>Non-integrated interventions</b> is the default strategy and the status quo. ' +
+			'It refers to the schedule the the user is currently following for distributing medical countermeasures and vector control interventions.';
+		var intContentStr = '<b>Integrated interventions</b> entails distributing medical countermeasures and vector control interventions ' +
+			'according to a schedule which the literature suggests is effective for reducing the prevalence of malaria and schistosomiasis.';
+		var recContentStr = 'This is the <b>recommended</b> strategy for the user.';
+		if (isRecommended) intContentStr += ' ' + recContentStr;
+		else nonIntContentStr += ' ' + recContentStr;
+		$('.output-recommendation .explanation-icon').tooltipster({
+			maxWidth: 450,
+			contentAsHTML: true,
+			content: nonIntContentStr + '<br><br>' + intContentStr
 		});
 		
-		var barData = [
-			{type: 'without integration', disease: 'schisto', value: schistoPrevNoInt},
-			{type: 'without integration', disease: 'malaria', value: malariaPrevNoInt},
-			{type: 'with integration', disease: 'schisto', value: schistoPrevInt},
-			{type: 'with integration', disease: 'malaria', value: malariaPrevInt}
-		];
+		// fill table showing prevalence
+		d3.selectAll('.output-table tbody tr').each(function(d, i) {
+			var outputs = (i === 0) ? App.outputs.separate : App.outputs.integrated;
+			
+			// populate cells with prevalence value
+			var cells = d3.select(this).selectAll('td:nth-child(2), td:nth-child(3)').text(function(dd, j) {
+				var val = (j === 0) ? outputs.schisto : outputs.malaria;
+				return Util.percentize(val);
+			});
+			
+			// apply green or red to "with integration" cells
+			if (i === 1) {
+				cells
+					.classed('text-success', function(dd, j) {
+						var disease = (j === 0) ? 'schisto' : 'malaria';
+						return (Math.round(100*App.outputs.separate[disease]) - Math.round(100*App.outputs.integrated[disease]) >= 2);
+					})
+					.classed('text-danger', function(dd, j) {
+						var disease = (j === 0) ? 'schisto' : 'malaria';
+						return (Math.round(100*App.outputs.integrated[disease]) - Math.round(100*App.outputs.separate[disease]) >= 2);
+					});
+			}
+		});
 		
 		// build bar chart for the population age distribution
-		var margin = {top: 20, right: 20, bottom: 80, left: 80};
+		var barData = [
+			{type: 'without integration', disease: 'schisto', value: App.outputs.separate.schisto},
+			{type: 'without integration', disease: 'malaria', value: App.outputs.separate.malaria},
+			{type: 'with integration', disease: 'schisto', value: App.outputs.integrated.schisto},
+			{type: 'with integration', disease: 'malaria', value: App.outputs.integrated.malaria}
+		];
+		
+		var margin = {top: 10, right: 20, bottom: 80, left: 80};
 		var width = 650 - margin.left - margin.right;
 		var height = 300 - margin.top - margin.bottom;
    		var chart = d3.select('.output-bar-chart')
@@ -79,7 +99,7 @@ var App = App || {};
 		var yAxisG = chart.append('g')
 			.attr('class', 'y axis')
 			.call(yAxis);
-				
+		
 		// bars
 		var bars = chart.selectAll('rect')
 			.data(barData);
@@ -113,27 +133,53 @@ var App = App || {};
 		legendGroups.append('text')
 			.attr('transform', 'translate(25,13)')
 			.text(function(d) { return d; });
+		
+		
+		// add tooltips to question marks
+		$('.output-table-container .explanation-icon:first-child').tooltipster({
+			maxWidth: 400,
+			contentAsHTML: true,
+			content: 'The values shown are prevalence values for schistosomiasis and malaria when ' +
+				'<b>non-integrated interventions</b> are used'
+		});
+		$('.output-table-container .explanation-icon:nth-child(2)').tooltipster({
+			maxWidth: 450,
+			contentAsHTML: true,
+			content: 'The values shown are prevalence values for schistosomiasis and malaria when ' +
+				'<b>integrated interventions</b> are used. ' +
+				'<br><br>Values are colored <b class="text-success">green</b> if there is a non-trival reduction in prevalence (> 1% reduction). ' + 
+				'<br><br>Values are colored <b class="text-danger">red</b> if there is a non-trivial increase in prevalence (> 1% increase).'
+		});
 			
 		
 		/* ----------------------- Distribution Strategy Timeline ------------------------------- */
-		var timelineMargin = {top: 30, right: 20, bottom: (isSeasonal ? 100 : 65), left: 105};
+		var currMonthsEqualRec = (+App.inputs.schisto_month_num === +recOutput.pzq_month &&
+			+App.inputs.irs_month_num === +recOutput.spray_month &&
+			+App.inputs.itn_month_num === +recOutput.net_month);
+		var timelineMargin = {top: 20, right: 105, bottom: (isSeasonal ? 105 : 70), left: 105};
 		var timelineWidth = 800 - margin.left - margin.right;
-		var timelineHeight = 120;
+		var timHeight = 60;
+		var timSep = 20;
+		var timelineHeight = currMonthsEqualRec ? timHeight : 2*timHeight + timSep;
 		var timeline = d3.select('.output-timeline')
 			.attr('width', timelineWidth + timelineMargin.left + timelineMargin.right)
 			.attr('height', timelineHeight + timelineMargin.top + timelineMargin.bottom)
 			.append('g')
 				.attr('transform', 'translate(' + timelineMargin.left + ',' + timelineMargin.top + ')');
-				
-		timeline.append('rect')
-			.attr('class', 'timeline-base-current')
+		
+		var timelineCurr = timeline.append('g')
+			.attr('class', 'timeline-group');
+		var timelineRec = timeline.append('g')
+			.attr('class', 'timeline-group')
+			.attr('transform', 'translate(0,' + (timHeight+timSep) + ')');
+		timelineCurr.append('rect')
+			.attr('class', 'timeline-base')
 			.attr('width', timelineWidth)
-			.attr('height', timelineHeight/2);
-		timeline.append('rect')
-			.attr('class', 'timeline-base-rec')
-			.attr('y', timelineHeight/2)
+			.attr('height', timHeight);
+		timelineRec.append('rect')
+			.attr('class', 'timeline-base')
 			.attr('width', timelineWidth)
-			.attr('height', timelineHeight/2);
+			.attr('height', timHeight);
 
 		// establish dates
 		var getMonthXCoord = function(monthNum, year) {
@@ -156,78 +202,114 @@ var App = App || {};
 			.attr('class', 'x axis')
 			.attr('transform', 'translate(0,' + timelineHeight + ')')
 			.call(timelineXAxis);
+
+		// adjust axes labels
+		$('.output-timeline .x.axis .tick text')
+			.attr('x', timelineWidth/24)
+			.attr('y', 8)
+			.last()
+				.css('display', 'none');
 		
 		// add seasonal bar
 		if (isSeasonal) {
 			var malariaStartDate = getMonthXCoord(malariaStartMonthNum);
 			var malariaEndDate = getMonthXCoord(malariaEndMonthNum);
 			if (malariaEndDate < malariaStartDate) malariaEndDate = getMonthXCoord(malariaEndMonthNum, 2016);
-			timeline.append('rect')
+			timelineCurr.append('rect')
 				.attr('class', 'timeline-seasonal-base')
 				.attr('x', malariaStartDate)
 				.attr('width', malariaEndDate - malariaStartDate)
-				.attr('height', timelineHeight);
+				.attr('height', timHeight);
+			timelineRec.append('rect')
+				.attr('class', 'timeline-seasonal-base')
+				.attr('x', malariaStartDate)
+				.attr('width', malariaEndDate - malariaStartDate)
+				.attr('height', timHeight);
 		}
 			
 		// add labels
-		timeline.append('text')
+		timelineCurr.append('text')
 			.attr('class', 'y-label')
 			.attr('x', -10)
-			.attr('y', timelineHeight/4 + 3)
+			.attr('y', timHeight/2 + 3)
 			.text('current');
-		timeline.append('text')
+		timelineRec.append('text')
 			.attr('class', 'y-label')
 			.attr('x', -10)
-			.attr('y', 3*timelineHeight/4 + 3)
+			.attr('y', timHeight/2 + 3)
 			.text('recommended');
 		
 		// draw lines showing mitigation strategies
-		var circleRadius = 5;
-		var drawStrat = function(monthNum, className, text, yVal) {
+		var markerHeight = 10;
+		var drawStrat = function(t, monthNum, text, yVal) {
+			// calculate x-coordinates for the month supplied
+			if (typeof monthNum === 'string') monthNum = +monthNum;
 			var monthXCoord = getMonthXCoord(monthNum);
-			if (monthXCoord < 0) monthXCoord = getMonthXCoord(monthNum, 2016);
-			var stratGroup = timeline.append('g')
+			var nextMonthXCoord = getMonthXCoord(monthNum+1);
+			if (monthXCoord < 0) {
+				monthXCoord = getMonthXCoord(monthNum, 2016);
+				nextMonthXCoord = getMonthXCoord(monthNum+1, 2016);
+			}
+			
+			// draw the marker
+			var stratGroup = t.append('g')
 				.attr('class', 'strat-group')
 				.attr('transform', 'translate(' + monthXCoord + ',' + yVal + ')');
-			stratGroup.append('circle')
-				.attr('class', className)
-				.attr('r', circleRadius);
+			stratGroup.append('rect')
+				.attr('class', 'strat-marker')
+				.attr('y', -markerHeight/2)
+				.attr('width', nextMonthXCoord - monthXCoord)
+				.attr('height', markerHeight);
 			var label = stratGroup.append('text')
-				.attr('x', -10)
+				.attr('x', -7)
 				.attr('y', 4)
 				.text(text);
 			if (monthXCoord === 0) {
 				label
 					.style('text-anchor', 'start')
-					.attr('x', 10);
+					.attr('x', getMonthXCoord(malariaStartMonthNum-2) + 7);
 			}
 			return monthXCoord;
 		};
-		var currPzqX = drawStrat(App.inputs.schisto_month_num, 'current-strat-marker', 'PZQ', 12);
-		var currIrsX = drawStrat(App.inputs.irs_month_num, 'current-strat-marker', 'IRS', 30);
-		var currItnX = drawStrat(App.inputs.itn_month_num, 'current-strat-marker', 'ITN', 48);
-		var recPzqX = drawStrat(recOutput.pzq_month, 'rec-strat-marker', 'PZQ', 72);
-		var recIrsX = drawStrat(recOutput.spray_month, 'rec-strat-marker', 'IRS', 90);
-		var recItnX = drawStrat(recOutput.net_month, 'rec-strat-marker', 'ITN', 108);
 		
-		// draw line separating strategies
-		timeline.append('line')
+		var numTreats = App.inputs.itn + App.inputs.irs + 1;
+		var treatCoords = (numTreats === 2) ? [20, 40] : [12, 30, 48];
+		var currPzqX = drawStrat(timelineCurr, App.inputs.schisto_month_num, 'PZQ', treatCoords[0]);
+		var recPzqX = drawStrat(timelineRec, recOutput.pzq_month, 'PZQ', treatCoords[0]);
+		if (App.inputs.irs) {
+			var currIrsX = drawStrat(timelineCurr, App.inputs.irs_month_num, 'IRS', treatCoords[1]);
+			var recIrsX = drawStrat(timelineRec, recOutput.spray_month, 'IRS', treatCoords[1]);
+		}
+		if (App.inputs.itn) {
+			var currItnX = drawStrat(timelineCurr, App.inputs.itn_month_num, 'ITN', (numTreats === 2) ? treatCoords[1] : treatCoords[2]);
+			var recItnX = drawStrat(timelineRec, recOutput.net_month, 'ITN', (numTreats === 2) ? treatCoords[1] : treatCoords[2]);
+		}
+		
+		// draw borders around timelines
+		var timelineGroups = d3.selectAll('.timeline-group');
+		timelineGroups.append('line')
 			.attr('class', 'strat-divider')
 			.attr('x2', timelineWidth)
-			.attr('y1', timelineHeight/2)
-			.attr('y2', timelineHeight/2);
+			.attr('y1', 0)
+			.attr('y2', 0);
+		timelineGroups.append('line')
+			.attr('class', 'strat-divider')
+			.attr('x2', timelineWidth)
+			.attr('y1', timHeight)
+			.attr('y2', timHeight);
 			
 		// add legend
 		var timelineLegend = timeline.append('g')
 			.attr('class', 'timeline-legend')
-			.attr('transform', 'translate(0,' + (timelineHeight+45) + ')');
+			.attr('transform', 'translate(0,' + (timelineHeight+50) + ')');
 		if (isSeasonal) {
 			var seasonalGroup = timelineLegend.append('g')
 				.attr('transform', 'translate(222,0)');
 			seasonalGroup.append('rect')
 				.attr('class', 'timeline-seasonal-base')
+				.attr('y', -1)
 				.attr('width', 60)
-				.attr('height', 14);
+				.attr('height', 16);
 			seasonalGroup.append('text')
 				.attr('class', 'legend-text')
 				.attr('x', 75)
@@ -235,40 +317,44 @@ var App = App || {};
 				.text('Malaria Season');
 		}
 		var legendStratGroup = timelineLegend.append('g')
-			.attr('transform', 'translate(220,' + (isSeasonal ? 30 : 0) + ')');
-		legendStratGroup.append('circle')
-			.attr('class', 'current-strat-marker')
-			.attr('r', circleRadius)
-			.attr('cy', 7);
+			.attr('transform', 'translate(222,' + (isSeasonal ? 30 : 0) + ')');
+		legendStratGroup.append('rect')
+			.attr('class', 'strat-marker')
+			.attr('y', 2)
+			.attr('width', 60)
+			.attr('height', markerHeight);
 		legendStratGroup.append('text')
-			.attr('x', circleRadius+10)
+			.attr('x', 75)
 			.attr('y', 12)
 			.text('Distribution of Treatment');
 
 
 		// populate timeline recommendation text
-		var recText = '';
-		var treats = [];
-		if (recPzqX < currPzqX) treats.push('<b>PZQ</b>');
-		if (recIrsX < currIrsX) treats.push('<b>IRS</b>');
-		if (recItnX < currItnX) treats.push('<b>ITN</b>');
-		if (treats.length > 0) {
-			var ttext = (treats.length === 1) ? 'treatment' : 'treatments';
-			var verb = (treats.length === 1) ? 'is' : 'are';
-			recText += 'It is recommended that the ' + ttext + ', ' + treats.join(', ') + ', ' + verb + ' applied at an earlier time.';
-		}
-
-		treats = [];
-		if (recPzqX > currPzqX) treats.push('<b>PZQ</b>');
-		if (recIrsX > currIrsX) treats.push('<b>IRS</b>');
-		if (recItnX > currItnX) treats.push('<b>ITN</b>');
-		if (treats.length > 0) {
-			if (recText.length > 0) recText += '<br>';
-			var etext = (treats.length === 1) ? 'treatment' : 'treatments';
-			var verb = (treats.length === 1) ? 'is' : 'are';
-			recText += 'It is recommended that the ' + etext + ', ' + treats.join(', ') + ', ' + verb + ' applied at a later time.';
-		}
-		if (recPzqX !== currPzqX || recIrsX !== currIrsX || recItnX !== currItnX) {
+		if (currMonthsEqualRec) {
+			$('.output-timeline-rec-text').html('The <b>current</b> distribution times used for PZQ, IRS, and ITN are also the <b>recommended</b> distribution times.');
+			timelineGroups.style('display', function(d, i) { if (i === 1) return 'none'; });
+		} else {
+			var recText = '';
+			var treats = [];
+			if (recPzqX < currPzqX) treats.push('<b>PZQ</b>');
+			if (recIrsX < currIrsX) treats.push('<b>IRS</b>');
+			if (recItnX < currItnX) treats.push('<b>ITN</b>');
+			if (treats.length > 0) {
+				var ttext = (treats.length === 1) ? 'treatment' : 'treatments';
+				var verb = (treats.length === 1) ? 'is' : 'are';
+				recText += 'It is recommended that the ' + ttext + ', ' + treats.join(', ') + ', ' + verb + ' applied at an earlier time.';
+			}
+	
+			treats = [];
+			if (recPzqX > currPzqX) treats.push('<b>PZQ</b>');
+			if (recIrsX > currIrsX) treats.push('<b>IRS</b>');
+			if (recItnX > currItnX) treats.push('<b>ITN</b>');
+			if (treats.length > 0) {
+				if (recText.length > 0) recText += '<br>';
+				var etext = (treats.length === 1) ? 'treatment' : 'treatments';
+				var verb = (treats.length === 1) ? 'is' : 'are';
+				recText += 'It is recommended that the ' + etext + ', ' + treats.join(', ') + ', ' + verb + ' applied at a later time.';
+			}
 			$('.output-timeline-rec-text').html(recText);
 		}
 
@@ -281,8 +367,12 @@ var App = App || {};
 		
 		// update control measure recommended execution times
 		d3.select('#rec_pzq_month').text(formatMonth(recOutput.pzq_month));
-		d3.select('#rec_net_month').text(formatMonth(recOutput.net_month));
-		d3.select('#rec_spray_month').text(formatMonth(recOutput.spray_month));
+		
+		if (App.inputs.irs) d3.select('#rec_spray_month').text(formatMonth(recOutput.spray_month));
+		else $('.output-strategy-table tbody tr:nth-child(2)').hide();
+		
+		if (App.inputs.itn) d3.select('#rec_net_month').text(formatMonth(recOutput.net_month));
+		else $('.output-strategy-table tbody tr:nth-child(3)').hide();
 		
 		// toggling display of assumptions
 		$('.assumption-bar').on('click', function() {
